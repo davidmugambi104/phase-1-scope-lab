95c18182b31152f1c21c49fb67297ed93eb748fe
davidmugambi104 <davidmugambi104@gmail.com>
2024-03-31 15:03:44 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'; // Declare and assign 'bob' to customerName in global scope
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); // Accesses global customerName and uppercases it
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declares bestCustomer in global scope and assigns 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; // Modifies bestCustomer in global scope to 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'someone'; // Declare leastFavoriteCustomer as a constant in global scope
+
+function changeLeastFavoriteCustomer() {
+    //reassign a constant should result in an error
+    leastFavoriteCustomer = 'someone else'; 
+}
